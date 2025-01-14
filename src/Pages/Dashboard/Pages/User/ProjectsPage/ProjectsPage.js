660c2aebacdce8bbb68f4c263c32247f2b81e642
idriss.neumann@comwork.io <idriss.neumann@comwork.io>
2025-01-14 16:27:48 +0000

    Release: ce-3.11.14-297c0fbb

--- src/Pages/Dashboard/Pages/User/ProjectsPage/ProjectsPage.js
+++ src/Pages/Dashboard/Pages/User/ProjectsPage/ProjectsPage.js
@@ -23,7 +23,7 @@ import filteredListWithoutRemovedElement from "../../../../../utils/filter";
 
 function ProjectsPage(props) {
     const context = useContext(GlobalContext);
-    const currentPath = location.pathname
+    const currentPath = useLocation()
     const is_admin = currentPath.includes('admin')
     const createLink = is_admin ? "/admin/projects/create" : "/projects/create"
     const [projects, setProjects] = useState([]);
